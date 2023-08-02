80d23c72b4a7be48bcdc1cc4ac81ee005cd81977
ToJoos <joosx2000@gmail.com>
2023-08-02 17:56:53 +0200

    Add currencyRequest pojo

---
+++ src/main/java/tojoos/currencychecker/pojo/CurrencyRequest.java
@@ -0,0 +1,27 @@
+package tojoos.currencychecker.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class CurrencyRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String currencyCode;
+    private String requesterName;
+    private LocalDateTime date;
+    private Double currencyValue;
+}
