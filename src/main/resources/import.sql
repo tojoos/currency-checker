f29d016af37d9366dea80b041f15b6269e69e006
ToJoos <joosx2000@gmail.com>
2023-08-02 18:03:38 +0200

    Add properties files and sample data in .sql format

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO currency_checkerDB.currency_request (id, currency_code, currency_value, date, requester_name)
+VALUES  ('1', 'USD', '4.0497', '2023-08-02 11:46:13.461270', 'Jan Kowalski'),
+        ('2', 'CHF', '4.5493', '2023-08-02 12:22:13.221270', 'Marek Krakowiak'),
+        ('3', 'USD', '4.0291', '2023-08-01 21:56:11.534270', 'Paweł Gaweł'),
+        ('4', 'EUR', '4.4411', '2023-08-01 11:46:13.461270', 'Jan Kowalski'),
+        ('5', 'USD', '4.0497', '2023-08-01 05:41:22.838270', 'Jan Kowalski');
